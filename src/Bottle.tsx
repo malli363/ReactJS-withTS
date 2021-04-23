655cfb3d26b9ee9c5f15bbc27378cf740dc74172
mallikarjuna <mallik.kokkandla@gmail.com>
2021-04-23 17:13:57 +0530

    BottleApplication

---
+++ src/Bottle.tsx
@@ -0,0 +1,15 @@
+import React from "react";   
+interface IState{
+
+};
+interface IProps{
+
+};
+class Bottle extends React.Component <IProps,IState>{
+    render(){
+        return(
+            <h1> Welcome to COVID-19</h1>
+        )
+    }
+};
+export default Bottle;
